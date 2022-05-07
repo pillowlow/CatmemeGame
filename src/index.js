1311f573866d489c049b325da69af9c48a7ec598
pillowlow <tmps9930613@gmail.com>
2022-05-08 01:59:17 +0800

    score text complete

--- src/index.js
+++ src/index.js
@@ -33,6 +33,19 @@ var Cloud;
 var Clouds = new Array();
 var platform;
 var timer;
+var playerComBody = {
+  PositionX: null,
+  PositionY: null,
+  VelocityX: null,
+  VelocityY: null,
+  Rotation: 0,
+  AddedForce: {x:0,y:0},
+  Bodies: new Array(),
+  PostOffset: new Array(),
+
+
+
+};
 var player1;
 var player2;
 var cursors;
@@ -40,13 +53,16 @@ var nails= new Array();
 var score = 0;
 var scoreText;
 var bombs = new Array();
+var bombCtr =0;
 var moneys = new Array();
 var ColliderCats = {
   Noncollide : null,
   staticCat : null,
   playerCat : null,
   bombCat : null,
+  boalCat : null,
   moneyCat : null,
+ 
 };
 
 
@@ -84,6 +100,7 @@ class MyGame extends Phaser.Scene {
   }
   update() {
     
+
     /*
     if (cursors.left.isDown) {
       player.setVelocityX(-160);
@@ -109,8 +126,8 @@ class MyGame extends Phaser.Scene {
     //player 2 gelen dataya göre haraket ettir
     timer ++ ;
     
-
-    // Cloud
+    this.playerUpdate();
+    
 
     Clouds.forEach(element => {
       if(element.x > 1200){
@@ -120,22 +137,20 @@ class MyGame extends Phaser.Scene {
       };
     });
 
+    scoreText.setText(score);
     
 
-    if(timer % 200 ==0){
+    if(timer % 300 ==0){
       if(bombs.length<20){
         // bombCreate
         this.bombCreate();
-        
-        
       }
       this.moneyCreate();
       
     }
-
-
     this.BombManaging();
 
+    
 
 
   }
@@ -147,6 +162,7 @@ class MyGame extends Phaser.Scene {
     ColliderCats.playerCat = this.matter.world.nextCategory();
     ColliderCats.bombCat = this.matter.world.nextCategory();
     ColliderCats.moneyCat = this.matter.world.nextCategory();
+    ColliderCats.boalCat = this.matter.world.nextCategory();
 
   }
   animationCreate(){
@@ -216,17 +232,18 @@ class MyGame extends Phaser.Scene {
     cursors = this.input.keyboard.createCursorKeys();
     this.bombCreate();
 
-    // COLLIDION
     
-
-
-
-    
-    
-    scoreText = this.add.text(16, 16, `score : 0`, {
+    scoreText = this.add.text(16, 16, score, {
       fontSize: "32px",
       fill: "#000",
     });
+    console.log(scoreText);
+
+
+
+    
+    
+    
 
     
     
@@ -248,27 +265,108 @@ class MyGame extends Phaser.Scene {
     
 
 
-    this.matter.add.image(-50,300,"sidewall").setScale(0.8).setStatic(true).setCollisionCategory(ColliderCats.staticCat);
-    this.matter.add.image(850,300,"sidewall").setScale(0.8).setStatic(true).setCollisionCategory(ColliderCats.staticCat);
+    this.matter.add.image(-50,300,"sidewall").setScale(0.8).setStatic(true).setCollisionCategory(ColliderCats.staticCat).setCollidesWith([ ColliderCats.bombCat, ColliderCats.playerCat]);;
+
+    this.matter.add.image(850,300,"sidewall").setScale(0.8).setStatic(true).setCollisionCategory(ColliderCats.staticCat).setCollidesWith([ ColliderCats.bombCat, ColliderCats.playerCat]);
     platform = this.matter.add.image(400, 750, "ground").setScale(0.8);
     platform.setStatic(true);
     platform.setCollisionCategory(ColliderCats.staticCat);
 
   }
   playersCreate(){
-    player1 = this.matter.add.sprite(400,500,"angercatsheet").setScale(0.2);
+
+    
+
+
+    player1 = this.matter.add.sprite(400,650,"angercatsheet").setScale(0.2);
     player1.setCollisionCategory(ColliderCats.playerCat);
-    player1.setCollidesWith([ ColliderCats.bombCat, ColliderCats.staticCat, ColliderCats.moneyCat]);
+    player1.setCollidesWith([ ColliderCats.bombCat, ColliderCats.staticCat]);
     player1.anims.play("left");
-    player2 = this.matter.add.sprite(450,500,"angercatsheet").setScale(0.2);
+    player1.flipX = true;
+
+    playerComBody.Bodies.push(player1);
+
+    player2 = this.matter.add.sprite(480,650,"angercatsheet").setScale(0.2);
     player2.anims.play("left");
     player2.setCollisionCategory(ColliderCats.playerCat);
-    player2.setCollidesWith([ ColliderCats.bombCat, ColliderCats.staticCat, ColliderCats.moneyCat]);
-    var boal = this.matter.add.sprite(425,450,"boal").setScale(0.2);
-    boal.setCollisionCategory(ColliderCats.playerCat);
-    boal.setCollidesWith([ ColliderCats.bombCat, ColliderCats.staticCat, ColliderCats.moneyCat]);
+    player2.setCollidesWith([ ColliderCats.bombCat, ColliderCats.staticCat]);
+
+    playerComBody.Bodies.push(player2);
+    
+    var boal = this.matter.add.sprite(440,625,"boal").setScale(0.2).setStatic(true);
+    boal.setCollisionCategory(ColliderCats.boalCat);
+    boal.setCollidesWith([ ColliderCats.bombCat,  ColliderCats.moneyCat]);
+
+    this.matterCollision.addOnCollideStart({
+      objectA: boal,
+      //objectB: platform,
+      callback: function(eventData) {
+        console.log("collide");
+        
+      },
+      context: this 
+    });
+
+
+    playerComBody.Bodies.push(boal);
+
+
+
+    playerComBody.PositionX = (player1.x+player2.x+boal.x)/3;
+    playerComBody.PositionY = (player1.y+player2.y+boal.y)/3;
+
+    playerComBody.PostOffset.push({label: player1, x: player1.x-playerComBody.PositionX, y: player1.y-playerComBody.PositionY });
+    playerComBody.PostOffset.push({label: player2, x: player2.x-playerComBody.PositionX, y: player2.y-playerComBody.PositionY });
+    playerComBody.PostOffset.push({label: boal, x: boal.x-playerComBody.PositionX, y: boal.y-playerComBody.PositionY });
+
+    
+
+    
+
+    
+  }
+
+  playerUpdate(){
+    
+    
+    playerComBody.VelocityX = 0;
+    playerComBody.VelocityY = 0;
+
+    if (cursors.left.isDown ) {
+      playerComBody.VelocityX = -5;
+    }
+    if (cursors.right.isDown) {
+      playerComBody.VelocityX = 5;
+    }
+
+    for(let i=0; i<playerComBody.Bodies.length;i++){
+
+      playerComBody.Bodies[i].setPosition(playerComBody.PositionX+playerComBody.PostOffset[i].x,playerComBody.PositionY+playerComBody.PostOffset[i].y);
+      playerComBody.Bodies[i].body.angle = 0;
+    }
+    if(timer%20>10){
+      playerComBody.Bodies[2].setPosition(playerComBody.PositionX+playerComBody.PostOffset[2].x,playerComBody.PositionY+playerComBody.PostOffset[2].y+3);
+    }
+
+    if(playerComBody.VelocityX != 0 || playerComBody.VelocityY != 0){
+      playerComBody.PositionX += playerComBody.VelocityX;
+      playerComBody.PositionY += playerComBody.VelocityY;
+
+      if(playerComBody.PositionX>800){
+        playerComBody.PositionX = 800;
+      }
+      else if(playerComBody.PositionX<0){
+        playerComBody.PositionX = 0;
+      }
+      
+    }
+
+
+    
+
 
   }
+
 
   
   nailCreate(){
@@ -359,9 +457,12 @@ class MyGame extends Phaser.Scene {
     bomb.setFriction(0.005);
     bomb.anims.play("bomb-idle");
     bomb.deadCtr = 0;
+    bomb.num = bombCtr;
+    bombCtr++;
+
     
     bomb.setCollisionCategory(ColliderCats.bombCat);
-    bomb.setCollidesWith([ ColliderCats.bombCat,ColliderCats.playerCat,ColliderCats.staticCat]);
+    bomb.setCollidesWith([ ColliderCats.bombCat,ColliderCats.playerCat,ColliderCats.bombCat,ColliderCats.staticCat,ColliderCats.boalCat]);
 
 
 
@@ -395,6 +496,19 @@ class MyGame extends Phaser.Scene {
       },
       context: this 
     });
+    this.matterCollision.addOnCollideEnd({
+      objectA: bomb,
+      objectB: playerComBody.Bodies[2],
+      callback: function(eventData) {
+
+        const { bodyA, bodyB, gameObjectA, gameObjectB, pair } = eventData;
+        bomb.deadCtr = 600;
+        
+      },
+      context: this 
+    });
+
+    
   
 
 
@@ -417,9 +531,23 @@ class MyGame extends Phaser.Scene {
     
     
     money.setCollisionCategory(ColliderCats.moneyCat);
-    money.setCollidesWith([ ColliderCats.playerCat ]);
+    money.setCollidesWith([ ColliderCats.boalCat ]);
     money.setVelocityY(2);
     money.setFrictionAir(0);
+
+    this.matterCollision.addOnCollideEnd({
+      objectA: money,
+      
+
+      callback: function(eventData) {
+        
+        const { bodyA, bodyB, gameObjectA, gameObjectB, pair } = eventData;
+        money.destroy();
+        score++;
+        
+      },
+      context: this 
+    });
     
 
 
@@ -472,10 +600,13 @@ class MyGame extends Phaser.Scene {
       else if( 600 <= element.deadCtr){
         
         
-        
         this.ExplosionCreate(element.body.position.x,element.body.position.y);
+        
+        
+        var tempar = this.RemoveEle(bombs,element);
+        bombs = tempar;
         element.destroy();
-        bombs.shift();
+        
         
       }
 
@@ -520,6 +651,17 @@ class MyGame extends Phaser.Scene {
 
       sendDeadData();
     }*/
+  }
+  RemoveEle(arr,element){
+    
+    var result = [];
+    arr.forEach(function (target){
+      if(target != element){
+        result.push(target);
+        
+      }
+    });
+    return result;
   }
 
 }
